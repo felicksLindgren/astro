295def9ea15c49b9c9b4e8b35c8a87fe8e2f4fac
Felix Lindgren <felix.p.lindgren@gmail.com>
2023-04-27 11:25:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'http://beta.integrationsbolaget.com/'
+    site: 'https://felickslindgren.github.io/'
 });
